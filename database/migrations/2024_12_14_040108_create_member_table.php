291a6b869d9acd23e5f7003291f9690b01861a46
w1y0n0 <wiyono30@gmail.com>
2024-12-14 12:22:30 +0700

    buat database migration

---
+++ database/migrations/2024_12_14_040108_create_member_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member', function (Blueprint $table) {
+            $table->increments('id_member');
+            $table->string('kode_member', 25)->unique();
+            $table->string('nama', 50);
+            $table->text('alamat')->nullable();
+            $table->string('telepon', 13);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member');
+    }
+};
